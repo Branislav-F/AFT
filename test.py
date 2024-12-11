eb31739a094e33a55d8f128092f0eb547c7ef9d8
Brano <branislav.fecko@gmail.com>
2024-12-11 22:55:46 +0100

    Add random data generation and output to test.py

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
 print("hello world")
 #test 
+
+#loop for generating random data and save it to tuples
+import random   
+data = []
+for _ in range(10):
+    tup = (random.randint(1, 100), random.random())
+    data.append(tup)
+
+print(data)
